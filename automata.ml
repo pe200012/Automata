45762295ffb30f4664690e0e04cab590733e65a9
pe200012 <1326263755@qq.com>
2018-01-31 13:20:04 +0800

    -

--- automata.ml
+++ automata.ml
@@ -1,40 +1,87 @@
 open Core_kernel
 
-module Automata = struct
-  exception Unknown_node
-  exception Illegal_action of string
-  exception Exn_pair of exn * exn
+exception Unknown_node
+exception Illegal_action of string
+exception Exn_pair of exn * exn
 
-  module type S = sig
-    type ('stat,'arg,'result) t
-    type ('stat,'arg,'result) node
-    val create  : ('a * ('a,'b,'c) node) list -> ('a,'b,'c) t
-    val add     : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
-    val replace : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
-    val remove  : ('a,'b,'c) t -> 'a -> unit
-    val clear   : ('a,'b,'c) t -> unit
-    val run     : (unit -> 'a * 'b) -> ('a,'b,'c) t -> 'c
-  end
+module type S = sig
+  type ('stat,'arg,'result) t
+  type ('stat,'arg,'result) node
+  val create  : ('a * ('a,'b,'c) node) list -> ('a,'b,'c) t
+  val add     : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
+  val replace : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
+  val remove  : ('a,'b,'c) t -> 'a -> unit
+  val clear   : ('a,'b,'c) t -> unit
+  val run     : (unit -> 'a * 'b) -> ('a,'b,'c) t -> 'c
+end
 
-  module A1 : sig
+module A1 : sig
+  type ('stat,'arg,'result) node =
+    {
+      f : [ `Node of 'arg -> 'stat * 'arg | `Final of 'arg -> 'result];
+      comment : string
+    }
+  include S with type ('stat,'arg,'result) node := ('stat,'arg,'result) node
+  val run  : (unit -> 'a * 'b) -> ?debug:bool -> ('a,'b,'c) t -> 'c
+end = struct
+
+  (** 自动机和节点类型 *)
+  type ('stat,'arg,'result) t = ('stat, ('stat,'arg,'result) node) Hashtbl.Poly.t
+  (** 使用哈希表来组织自动机，相对列表提高查询效率 *)
+  and ('stat,'arg,'result) node =
+    {
+      f : [ `Node of 'arg -> 'stat * 'arg | `Final of 'arg -> 'result];
+      comment   : string
+    }
+
+  let create (ns:('a*(_,_,_)node)list) =
+    let t = Hashtbl.Poly.create () in
+    List.iter ~f:(fun (key,data) ->
+        match Hashtbl.add t ~key ~data with
+        | `Duplicate -> raise (Illegal_action "Duplicated node")
+        | `Ok -> ()) ns;
+    t
+
+  let add t n =
+    let s, n = n in
+    match Hashtbl.add t ~key:s ~data:n with
+    | `Duplicate -> raise (Illegal_action "Duplicated node")
+    | `Ok -> ()
+
+  let replace t n = let key, data = n in Hashtbl.set t ~key ~data
+
+  let remove = Hashtbl.Poly.remove
+
+  let clear = Hashtbl.Poly.clear
+
+  let run (init_f:unit -> 'a * 'b) ?(debug=false) (t:('a,'b,'c)t) : 'c =
+    let rec loop n a =
+      match n.f with
+      | `Final f -> f a
+      | `Node f ->
+        trans (try f a with e -> raise (Exn_pair (Illegal_action "Terminated on non-final node",e)))
+    and trans (s,a) =
+      match Hashtbl.Poly.find t s with
+      | None -> raise Unknown_node
+      | Some n -> loop n a
+    in
+    trans (init_f ())
+
+  module A2 : sig
     type ('stat,'arg,'result) node =
       {
-        f : [ `Node of 'arg -> 'stat * 'arg | `Final of 'arg -> 'result];
-        comment : string
+        f : 'arg -> [`State of 'stat * 'arg | `Result of 'result];
+        comment : string (** For debug purpose *)
       }
     include S with type ('stat,'arg,'result) node := ('stat,'arg,'result) node
     val run  : (unit -> 'a * 'b) -> ?debug:bool -> ('a,'b,'c) t -> 'c
   end = struct
-
-    (** 自动机和节点类型 *)
     type ('stat,'arg,'result) t = ('stat, ('stat,'arg,'result) node) Hashtbl.Poly.t
-    (** 使用哈希表来组织自动机，相对列表提高查询效率 *)
     and ('stat,'arg,'result) node =
       {
-        f : [ `Node of 'arg -> 'stat * 'arg | `Final of 'arg -> 'result];
-        comment   : string
+        f : 'arg -> [`State of 'stat * 'arg | `Result of 'result];
+        comment : string (** For debug purpose *)
       }
-
     let create (ns:('a*(_,_,_)node)list) =
       let t = Hashtbl.Poly.create () in
       List.iter ~f:(fun (key,data) ->
@@ -57,10 +104,9 @@ module Automata = struct
 
     let run (init_f:unit -> 'a * 'b) ?(debug=false) (t:('a,'b,'c)t) : 'c =
       let rec loop n a =
-        match n.f with
-        | `Final f -> f a
-        | `Node f ->
-          trans (try f a with e -> raise (Exn_pair (Illegal_action "Terminated on non-final node",e)))
+        match (try n.f a with e -> raise (Exn_pair (Illegal_action "Terminated on non-final node",e))) with
+        | `Result r -> r
+        | `State s -> trans s
       and trans (s,a) =
         match Hashtbl.Poly.find t s with
         | None -> raise Unknown_node
@@ -68,53 +114,5 @@ module Automata = struct
       in
       trans (init_f ())
 
-    module A2 : sig
-      type ('stat,'arg,'result) node =
-        {
-          f : 'arg -> [`State of 'stat * 'arg | `Result of 'result];
-          comment : string (** For debug purpose *)
-        }
-      include S with type ('stat,'arg,'result) node := ('stat,'arg,'result) node
-      val run  : (unit -> 'a * 'b) -> ?debug:bool -> ('a,'b,'c) t -> 'c
-    end = struct
-      type ('stat,'arg,'result) t = ('stat, ('stat,'arg,'result) node) Hashtbl.Poly.t
-      and ('stat,'arg,'result) node =
-        {
-          f : 'arg -> [`State of 'stat * 'arg | `Result of 'result];
-          comment : string (** For debug purpose *)
-        }
-      let create (ns:('a*(_,_,_)node)list) =
-        let t = Hashtbl.Poly.create () in
-        List.iter ~f:(fun (key,data) ->
-            match Hashtbl.add t ~key ~data with
-            | `Duplicate -> raise (Illegal_action "Duplicated node")
-            | `Ok -> ()) ns;
-        t
-
-      let add t n =
-        let s, n = n in
-        match Hashtbl.add t ~key:s ~data:n with
-        | `Duplicate -> raise (Illegal_action "Duplicated node")
-        | `Ok -> ()
-
-      let replace t n = let key, data = n in Hashtbl.set t ~key ~data
-
-      let remove = Hashtbl.Poly.remove
-
-      let clear = Hashtbl.Poly.clear
-
-      let run (init_f:unit -> 'a * 'b) ?(debug=false) (t:('a,'b,'c)t) : 'c =
-        let rec loop n a =
-          match (try n.f a with e -> raise (Exn_pair (Illegal_action "Terminated on non-final node",e))) with
-          | `Result r -> r
-          | `State s -> trans s
-        and trans (s,a) =
-          match Hashtbl.Poly.find t s with
-          | None -> raise Unknown_node
-          | Some n -> loop n a
-        in
-        trans (init_f ())
-
-    end
   end
 end
--- automata.mli
+++ automata.mli
@@ -1,52 +1,50 @@
-module Automata : sig
 
-  exception Unknown_node
-  exception Illegal_action of string
-  exception Exn_pair of exn * exn
+exception Unknown_node
+exception Illegal_action of string
+exception Exn_pair of exn * exn
 
-  module type S = sig
-    (** The interface of automachine *)
+module type S = sig
+  (** The interface of automachine *)
 
-    (** type of automachine of node *)
-    type ('stat,'arg,'result) t
-    type ('stat,'arg,'result) node
+  (** type of automachine of node *)
+  type ('stat,'arg,'result) t
+  type ('stat,'arg,'result) node
 
-    (** [[create ns]] will create a automachine with type of ns's nodes' type *)
-    val create  : ('a * ('a,'b,'c) node) list -> ('a,'b,'c) t
+  (** [[create ns]] will create a automachine with type of ns's nodes' type *)
+  val create  : ('a * ('a,'b,'c) node) list -> ('a,'b,'c) t
 
-    val add     : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
+  val add     : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
 
-    val replace : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
+  val replace : ('a,'b,'c) t -> ('a * ('a,'b,'c) node) -> unit
 
-    val remove  : ('a,'b,'c) t -> 'a -> unit
+  val remove  : ('a,'b,'c) t -> 'a -> unit
 
-    val clear   : ('a,'b,'c) t -> unit
+  val clear   : ('a,'b,'c) t -> unit
 
-    (** [[run t]] will eventually return a result with type of t's result type. 
-        Will raise exception if the node except {b Final} raise a exception
-        @param debug default = false. Will print some infos if set.
-    *)
-    val run     : (unit -> 'a * 'b) -> ('a,'b,'c) t -> 'c
-  end
-
-  module A1 : sig
-    type ('stat,'arg,'result) node =
-      {
-        f : [ `Node of 'arg -> 'stat * 'arg | `Final of 'arg -> 'result];
-        comment : string (** For debug purpose *)
-      }
-    include S with type ('stat,'arg,'result) node := ('stat,'arg,'result) node
-    val run  : (unit -> 'a * 'b) -> ?debug:bool -> ('a,'b,'c) t -> 'c
-  end
-
-  module A2 : sig
-    type ('stat,'arg,'result) node =
-      {
-        f : 'arg -> [`State of 'stat * 'arg | `Result of 'result];
-        comment : string (** For debug purpose *)
-      }
-    include S with type ('stat,'arg,'result) node := ('stat,'arg,'result) node
-    val run  : (unit -> 'a * 'b) -> ?debug:bool -> ('a,'b,'c) t -> 'c
-  end
-
+  (** [[run t]] will eventually return a result with type of t's result type. 
+      Will raise exception if the node except {b Final} raise a exception
+      @param debug default = false. Will print some infos if set.
+  *)
+  val run     : (unit -> 'a * 'b) -> ('a,'b,'c) t -> 'c
 end
+
+module A1 : sig
+  type ('stat,'arg,'result) node =
+    {
+      f : [ `Node of 'arg -> 'stat * 'arg | `Final of 'arg -> 'result];
+      comment : string (** For debug purpose *)
+    }
+  include S with type ('stat,'arg,'result) node := ('stat,'arg,'result) node
+  val run  : (unit -> 'a * 'b) -> ?debug:bool -> ('a,'b,'c) t -> 'c
+end
+
+module A2 : sig
+  type ('stat,'arg,'result) node =
+    {
+      f : 'arg -> [`State of 'stat * 'arg | `Result of 'result];
+      comment : string (** For debug purpose *)
+    }
+  include S with type ('stat,'arg,'result) node := ('stat,'arg,'result) node
+  val run  : (unit -> 'a * 'b) -> ?debug:bool -> ('a,'b,'c) t -> 'c
+end
+
